dfd9857823d010f51f38a3b3b69945d77d38c269
anupamakoirala <anukoirala.96@hotmail.com>
2023-05-17 17:09:43 +0545

    Pausable feature added

--- contracts/Erc1155.sol
+++ contracts/Erc1155.sol
@@ -3,10 +3,11 @@
 pragma solidity 0.8.10;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/security/Pausable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/utils/math/Math.sol';
 
-contract Reward is ERC1155{
+contract Reward is ERC1155, Pausable{
     using Counters for Counters.Counter;
     using Math for uint256;
 
@@ -32,7 +33,7 @@ contract Reward is ERC1155{
         _;
     }
 
-    function createToken(uint256 _initialSupply, uint256 _tokenReserve) public onlyAdmin() returns(uint256) {
+    function createToken(uint256 _initialSupply, uint256 _tokenReserve) public onlyAdmin returns(uint256) {
         uint256 _id = _currentTokenId.current();
         creators[_id] = msg.sender;
         tokenSupply[_id] = _initialSupply;
@@ -42,14 +43,14 @@ contract Reward is ERC1155{
         return _id;
     }
 
-    function mintToken(address _to ,uint256 _id, uint256 _quantity) public{
+    function mintToken(address _to ,uint256 _id, uint256 _quantity) public whenNotPaused{
         require(_exists(_id),"Token doesnot exists");
         require(tokenReserve[_id]>tokenSupply[_id] + _quantity,"Token limit hit");
         tokenSupply[_id] = tokenSupply[_id]+_quantity;
         _mint(_to, _id, _quantity, '');
     }
 
-    function batchMint(address _to, uint256[] memory _ids, uint256[] memory _quantity) public{
+    function batchMint(address _to, uint256[] memory _ids, uint256[] memory _quantity) public whenNotPaused {
         for(uint256 i =0; i< _ids.length; i++)
         {   
             require(_exists(i),"Token doesnot exists");
@@ -61,6 +62,14 @@ contract Reward is ERC1155{
 
     function _exists(uint256 _id) internal view returns(bool){
         return creators[_id] != address(0);
+    }
+
+    function pause() public onlyAdmin{
+        _pause();
+    }
+
+    function unpause() public whenPaused onlyAdmin{
+        _unpause();
     }
 }
 
